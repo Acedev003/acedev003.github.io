042486a1616a79edd9a98d73277a77077d6f804a
Acedev003 <acedeveloper003@gmail.com>
2022-01-30 21:34:55 +0530

    refactor: edit particle speed

--- assets/js/index.js
+++ assets/js/index.js
@@ -54,7 +54,7 @@ let params = {
       },
       "move": {
         "enable": true,
-        "speed": 6,
+        "speed": 3,
         "direction": "none",
         "random": false,
         "straight": false,
